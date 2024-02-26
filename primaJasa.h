6c87f8ed6251fb0094b1868008facfc3821df954
J-Aqsal <aqsalacc@gmail.com>
2024-02-26 10:16:24 +0700

    Header

---
+++ primaJasa.h
@@ -0,0 +1,11 @@
+#ifndef primaJasa_H
+#define primaJasa_H
+
+
+//fungsi untuk enkripsi plaintext/teks biasa diubah bentuknya
+char* enkripsi(char plainText[]);
+
+//fungsi untuk dekripsi ciphertext/menubah teks yang sudah diubah bentuknya menjadi teks sebelum diubah
+char* dekripsi(char cipherText[]);
+
+#endif
