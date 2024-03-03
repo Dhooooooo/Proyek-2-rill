8f54aaad83a5d40fd1238dcce866f1faf3bbdb6c
Prima Aji Akbar <prima.aji.tif23@polban.ac.id>
2024-03-03 17:30:42 +0700

    rencana login

--- primaJasa.h
+++ primaJasa.h
@@ -1,6 +1,17 @@
 #ifndef primaJasa_H
 #define primaJasa_H
 
+void encrypt(char *text);
+
+void decrypt(char *text);
+
+void registerUser(char *username, char *password);
+
+int loginUser(char *username, char *password);
+
+void modifyUser(char *username, char *newPassword);
+
+int displayMenu();
 
 //fungsi untuk enkripsi plaintext/teks biasa diubah bentuknya
 char* enkripsi(char plainText[]);
