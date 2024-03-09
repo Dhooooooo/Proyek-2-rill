3fed8f5355fb29ea4bc6301f9b0df1f185c2699f
Hafidz Zaenul Ahkam <hafidz.zaenul.tif23@polban.ac.id>
2024-03-09 14:23:28 +0700

    Menambahkan fitur menu dan display list coupon

--- coupon_admin.c
+++ coupon_admin.c
@@ -8,6 +8,7 @@
 
 void createCoupon(char *coupon, char *discount);
 void encryptCoupon(char *coupon);
+void displayCouponList();
 
 void createCoupon(char *coupon, char *discount) {
     FILE *couponList;
@@ -28,18 +29,91 @@ void encryptCoupon(char *coupon) {
     }
 }
 
+void displayCouponList() {
+    FILE *couponList;
+    couponList = fopen("List_coupon.txt", "r");
+    if (!couponList) {
+        printf("File tidak ada.\n");
+        return;
+    }
+
+    char storedCoupon[MAX_COUPON_LENGTH];
+    char storedDiscount[MAX_DISCOUNT_LENGTH];
+
+    printf("\nLIST COUPON\n");
+    printf("============\n");
+
+    while (fscanf(couponList, "%s %s", storedCoupon, storedDiscount) != EOF) {
+		printf("Kode Kupon: %s | Diskon: %s | ", storedCoupon, storedDiscount);
+		encryptCoupon(storedCoupon);
+		printf("Kupon: %s\n", storedCoupon);
+    }
+
+    fclose(couponList);
+}
+
+void decryptCoupon(char *coupon) {
+    char *ptr = coupon;
+    while (*ptr != '\0') {
+        if (*ptr != ' ') {
+            *ptr = (*ptr - ENCRYPTION_KEY + 126) % 126; // ASCII printable characters range
+        }
+        ptr++;
+    }
+}
+
+int displayMenu() {
+    int choice;
+    printf("\nMENU\n");
+    printf("1. Create coupon\n");
+    printf("2. List coupon\n");
+    printf("3. Exit\n");
+    printf("Pilih menu: ");
+    
+    scanf("%d", &choice);
+    
+    return choice;
+}
+
 int main() {
     char coupon[MAX_COUPON_LENGTH];
     char discount[MAX_DISCOUNT_LENGTH];
+    int choice;
     
-    printf("Masukkan kode kupon: ");
-    scanf("%s", coupon);
-    printf("Masukkan diskon: ");
-    scanf("%s", discount);
+    do {
+        choice = displayMenu();
+        system("cls");
+        switch (choice) {
+            case 1:
+                // create coupon
+                printf("Masukkan kode kupon: ");
+                scanf("%s", coupon);
+                printf("Masukkan diskon: ");
+                scanf("%s", discount);
     
-    createCoupon(coupon, discount);
-    encryptCoupon(coupon);
-    printf("\nKode kupon: %s\n", coupon);
+                createCoupon(coupon, discount);
+                encryptCoupon(coupon);
+    
+                printf("\nKode kupon: %s\n", coupon);
+                printf("\nMasukkan nomor apapun untuk kembali ke menu: ");
+                scanf("%d", &choice);
+                system("cls");
+                break;
+            
+            case 2:
+                // List coupon
+                displayCouponList();
+                printf("\nMasukkan nomor apapun untuk kembali ke menu: ");
+                scanf("%d", &choice);
+                system("cls");
+                break;
+            
+            case 3:
+                printf("\nEXIT\n");
+                break;
+        }
+    } while (choice != 3);
     
     return 0;
 }
+
