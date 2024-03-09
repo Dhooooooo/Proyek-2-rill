8eedd863c1b5a61b98069e95602ef13deaf7dd97
Hafidz Zaenul Ahkam <hafidz.zaenul.tif23@polban.ac.id>
2024-03-10 03:15:26 +0700

    update encryption key

--- coupon.c
+++ coupon.c
@@ -4,7 +4,7 @@
 
 #define MAX_COUPON_LENGTH 10
 #define MAX_DISCOUNT_LENGTH 10
-#define ENCRYPTION_KEY 8
+#define ENCRYPTION_KEY_LENGTH 8
 
 void readCoupon(char *coupon, char *discount);
 void decryptCoupon(char *coupon);
@@ -50,7 +50,7 @@ void decryptCoupon(char *coupon) {
     char *ptr = coupon;
     while (*ptr != '\0') {
         if (*ptr != ' ') {
-            *ptr = (*ptr - ENCRYPTION_KEY + 126) % 126; // ASCII printable characters range
+            *ptr = (*ptr - ENCRYPTION_KEY_LENGTH + 126) % 126; // ASCII printable characters range
         }
         ptr++;
     }
