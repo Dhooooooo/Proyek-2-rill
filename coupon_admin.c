824034e9a81827c6c7da663d13d947f4f3918797
Hafidz Zaenul Ahkam <hafidz.zaenul.tif23@polban.ac.id>
2024-03-09 11:04:56 +0700

    Progress insyaallah 100%

--- coupon_admin.c
+++ coupon_admin.c
@@ -2,46 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_COUPON_LENGHT 10
-#define MAX_DISCOUNT_LENGHT 10
+#define MAX_COUPON_LENGTH 10
+#define MAX_DISCOUNT_LENGTH 10
 #define ENCRYPTION_KEY 8
 
-void createCoupon(char *coupon, char *diskon);
+void createCoupon(char *coupon, char *discount);
 void encryptCoupon(char *coupon);
 
-void createCoupon(char *coupon, char *diskon) {
-	FILE *couponList;
-	couponList = fopen("List_coupon.txt", "a");
-    fprintf(couponList, "%s %s\n", coupon, diskon);
-    
-    encryptCoupon(coupon);
+void createCoupon(char *coupon, char *discount) {
+    FILE *couponList;
+    couponList = fopen("List_coupon.txt", "a");
+
+    fprintf(couponList, "%s %s\n", coupon, discount);
     
     fclose(couponList);
 }
 
 void encryptCoupon(char *coupon) {
-	char *ptr = coupon;
+    char *ptr = coupon;
     while (*ptr != '\0') {
-		if (*ptr != ' ') {
-    		*ptr = (*ptr + ENCRYPTION_KEY) % 126; // ASCII printable characters range
+        if (*ptr != ' ') {
+            *ptr = (*ptr + ENCRYPTION_KEY) % 126; // ASCII printable characters range
         }
         ptr++;
-    return;
-	}
+    }
 }
 
 int main() {
-	char coupon[MAX_COUPON_LENGHT];
-	char diskon[MAX_DISCOUNT_LENGHT];
-	
-	printf("Masukkan kode kupon: ");
-	scanf("%s", &coupon);
-	printf("Masukkan diskon: ");
-	scanf("%s", &diskon);
-	
-	createCoupon(coupon, diskon);
-	
-	printf("\nKode kupon: %s\n", coupon);
-	
-	return 0;
+    char coupon[MAX_COUPON_LENGTH];
+    char discount[MAX_DISCOUNT_LENGTH];
+    
+    printf("Masukkan kode kupon: ");
+    scanf("%s", coupon);
+    printf("Masukkan diskon: ");
+    scanf("%s", discount);
+    
+    createCoupon(coupon, discount);
+    encryptCoupon(coupon);
+    printf("\nKode kupon: %s\n", coupon);
+    
+    return 0;
 }
+
