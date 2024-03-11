e573fd14ca4dc725b25aa4e281bce64f4ff3babb
J-Aqsal <158978201+J-Aqsal@users.noreply.github.com>
2024-03-11 16:07:26 +0700

    Add files via upload
    update tampilan

--- fiturPemesanan.c
+++ fiturPemesanan.c
@@ -3,75 +3,27 @@
 #include "primaJasa.h"
 
 int main() {
-
-	/*************** TAMPILAN USER **************/
-	char *prompt = (char *)malloc(10 * sizeof(char)); // yang akan dituliskan ke dalam file
-	int hargaPerMalam = 500000; // harga hotel per malam
-	
-    char *tanggalCheckIn = inputTanggalCheckIn();     
-    printf("Tanggal check-in: %s\n", tanggalCheckIn);
+	char username[] = "nama0"; // ini untuk nampilin username user
+	/*************** TAMPILAN USER SAAT MEMESAN **************/
+	pemesananHotel(username);
     
-    char *tanggalCheckOut = inputTanggalCheckOut(tanggalCheckIn);    
-    printf("Tanggal check-out: %s\n", tanggalCheckOut);
     
-    int totalHariMenginap = hitungHariMenginap(tanggalCheckIn, tanggalCheckOut);
+    /*######### PEMESANAN HOTEL GABISA DIGABUNG SAMA DISPLAY ANTRIAN ###########*/
     
-	int hargaMenginap = totalHariMenginap*hargaPerMalam;
-	
-	int noKamar = kamar();
-	
-	// Menampilkan hasil ke user
+	/*************** TAMPILAN VERIF OLEH ADMIN **************/
+    disPemesananAdmin();
+	int noAntrian;
+	printf("Nomor yang telah berhasil : ");scanf("%d", &noAntrian);
+	adminApprove(noAntrian);
 	system("cls");
-	printf("<user>\n");
-	printf("Tanggal CheckIn\t : %s\nTanggal CheckOut : %s\nTotal menginap\t : %d hari\nNo kamar\t : %d\n", tanggalCheckIn, tanggalCheckOut, totalHariMenginap, noKamar);
-	printf("Harga\t\t : Rp. ");disHarga(hargaMenginap);
-    
-    // Menggabungkan tanggal check-in dan check-out ke dalam variabel prompt
-	sprintf(prompt, "%s,%s,%d,%d,%d,\n", tanggalCheckIn, tanggalCheckOut, totalHariMenginap, hargaMenginap, noKamar);   
-	
-	// Enkripsi prompt
-    char* enkripsiPesanan = enkripsi(prompt);
+	printf("<ADMIN>");
+	disPemesananAdmin();
 
-    FILE *file = fopen("pemesanan.txt", "a"); // membuka dile dengan mode append atau mode menambahkan
-    if (file == NULL) {
-        printf("Gagal membuka file.\n");
-        return 0;
-    }
-	// tulis pesanan ke file
-    fprintf(file, "%s", enkripsiPesanan); // menulis ke file
+	/*************** TAMPILAN HISTORY USER **************/
+	printf("\n\n<username : %s>\n", username);
+    disPemesananUser(username);
 
-    fclose(file);
-    
-    
-    
-    /*************** TAMPILAN ADMIN **************/
-    int i;
-    
-    FILE *files = fopen("pemesanan.txt", "r");
-    if (files == NULL) {
-        printf("Gagal membuka file.\n");
-        return 1;
-    }
-
-    printf("\n\n<admin>\n");
-	printf("+-----------------------------------------------------------------+\n");
-    printf("| %-12s | %-12s | %-4s | %-16s | %-6s |\n", "   CheckIn", "  CheckOut", "Hari", "     Harga", "NoKamar");
-    printf("|--------------+--------------+------+------------------+---------|\n");
-
-    char line[100];
-    while (fgets(line, sizeof(line), files)) {
-    	char *dekripPemesanan = dekripsi(line);
-    	
-    	char *CI = strtok(dekripPemesanan, ",");
-    	char *CO = strtok(NULL, ",");
-    	char *hari = strtok(NULL, ",");
-    	char *harga = strtok(NULL, ",");
-    	char *no = strtok(NULL, ",");
-    	
-    	printf("| %-12s | %-12s | %-4s |", CI, CO, hari);printf(" Rp. ");disHarga(atoi(harga));printf("\t| %-7s |\n", no);
-    }
-    printf("+-----------------------------------------------------------------+\n");
-	fclose(files);
     return 0;
 }
+
 
