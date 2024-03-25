c0873efd34c714a1cd17df3c4c639e586cd89d79
Hafidz Zaenul Ahkam <hafidz.zaenul.tif23@polban.ac.id>
2024-03-25 20:32:37 +0700

    update Hafidz

---
+++ user/Hafidz.h
@@ -0,0 +1,15 @@
+#ifndef Hafidz_h
+#define Hafidz_h
+
+#define MAX_COUPON_LENGTH 10
+
+// Fungsi untuk melakukan readCoupon
+int readCoupon(char *coupon, int discount);
+
+// Fungsi untuk memanggil fitur kupon
+int kupon(char *coupon);
+
+// Prosedur untuk melakukan cek kupon
+void checkCoupon (char *username);
+
+#endif
