edbcbb19261563f61384cf1d3a6be327bb3621ba
Prima Aji Akbar <prima.aji.tif23@polban.ac.id>
2024-05-20 07:54:02 +0700

    nambah

--- user/Prima.h
+++ user/Prima.h
@@ -16,8 +16,8 @@ void encrypt(char *text);
 void decrypt(char *text);
 
 //Akun Register Login
-void registerUser(char *username, char *password, char *pin);
-int loginUser(char *username, char *password, char *pin);
+void registerUser(char *username, char *password, char *pin, char *role);
+int loginUser(char *username, char *password, char *pin, char *role);
 void modifyPass(char *username, char *password);
 void modifyPin(char *username, char *newPin);
 void modifyUser(char *username, char *password);
